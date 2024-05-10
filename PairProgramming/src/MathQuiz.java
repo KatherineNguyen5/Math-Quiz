49b1a889dbdfc028de5fc8b3bb84ff4e6b70ffae
KatherineNguyen5 <159823835+KatherineNguyen5@users.noreply.github.com>
2024-05-10 15:18:29 -0400

    Math Quiz for grade 8

--- PairProgramming/src/MathQuiz.java
+++ PairProgramming/src/MathQuiz.java
@@ -7,32 +7,43 @@ public class MathQuiz {
     public void solve(){
         //declare variables
         String message = "Start Quiz!";
-        String title = "Simple functions";
-        int solve = Integer.parseInt(JOptionPane.showInputDialog(message, title));
+        String title = "Simple intro quiz for Linear Functions";
+        String notice = "Answer here, numbers only or program wont run";
+        String question1 = "Find the value of 'x' in the equation f(x)= 2x-8, x=?";
+        JOptionPane.showMessageDialog(null, title, message, 0);
+        int solve = Integer.parseInt(JOptionPane.showInputDialog(question1, notice));
 
         //while loop - instructing until the user solves the equation
-        String question1 = "Find the value of 'x' in the equation f(x)= 2x-8";
         while(solve!= 4){
-                message = "Incorrect.Think again. ";
-                message = "hint: Try isolating the x on one side";
-            } //read user next answer
-            solve = Integer.parseInt(JOptionPane.showInputDialog( message, title));
-            JOptionPane.showMessageDialog(null, question1);
+            message = "Incorrect.Think again. ";
+            notice = "hint: Try isolating the x on one side";
+            //read user next answer
+            JOptionPane.showMessageDialog(null, message);   
+            solve = Integer.parseInt(JOptionPane.showInputDialog( question1, notice));
         }
         //Message when correct
         message = "Excellent! bravo!";
-        message = "Next question";
+        JOptionPane.showMessageDialog(null, message);
+        message = "Next Question";
         JOptionPane.showMessageDialog(null, message);
 
-        //Second question
-        String question2 = "";
-        while(solve!= 4){
-                message = "Incorrect.Think again. ";
-                message = "hint: Try isolating the x on one side";
-            } //read user next answer
-            solve = Integer.parseInt(JOptionPane.showInputDialog( message, title));
-            JOptionPane.showMessageDialog(null, question1);
+        String question2 = "Find the valuse of 'x' in the equation = 3(x+21)+6x=10-2(x+4), to the nearest hundredth value";
+        solve = Integer.parseInt(JOptionPane.showInputDialog( question2, notice));
+
+    //while loop - instructing until the user solves the equation
+    while(solve!= -5.55){
+        message = "Incorrect.Think again. ";
+        notice = "hint: Expand and isolating the x on one side";
+        //read user next answer
+        JOptionPane.showMessageDialog(null, message);   
+        solve = Integer.parseInt(JOptionPane.showInputDialog( question2, notice));
     }
+    //Message when correct
+    message = "Excellent! bravo!";
+    JOptionPane.showMessageDialog(null, message);
 
-
+    //Ending message
+    message = "End of quiz! Congrats";
+    JOptionPane.showMessageDialog(null, message);
+    }
 }
